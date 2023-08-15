5010226c9db5273e1efd4fb3cb9f1bb766878b1b
Hugo Pereira <hugop1997@gmail.com>
2023-08-15 15:08:06 +0100

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+# The TasksController handles actions related to tasks in the application.
+class TasksController < ApplicationController
+
+  def index
+      # Your code here
+  end
+
+  def show
+      # Your code here
+  end
+
+  def new
+      # Your code here
+  end
+
+  def create
+      # Your code here
+  end
+
+  def edit
+      # Your code here
+  end
+
+  def update
+      # Your code here
+  end
+
+  def destroy
+      # Your code here
+  end
+end
